446e689e7b0e3643aed8c99bd10f45b232b0877e
Alican Kurt <kurta15@itu.edu.tr>
2021-08-11 19:45:51 +0300

    Initial Commit

---
+++ TravelBookwithCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TravelBookwithCoreData
+//
+//  Created by Alican Kurt on 11.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
